165a6d9ce07f55ae50cf8edc296c86606436f217
Akshita Jain <akshitajain963@gmail.com>
2023-06-18 19:05:08 +0530

    commit

--- NoteKeeper/models.py
+++ NoteKeeper/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Notekeeper modal where we have title and description as columns.
 
 class Notekeeper(models.Model):
     title = models.CharField(max_length=50)
--- NoteKeeper/views.py
+++ NoteKeeper/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from .models import Notekeeper
 from .serializers import NoteKeeperSerializer
@@ -10,3 +8,8 @@ class NoteKeeperView(viewsets.ModelViewSet):
     serializer_class = NoteKeeperSerializer
     queryset = Notekeeper.objects.all()
 
+# queryset : this to select data from database, you can select all data or apply filter or what you want,
+# in more basic way.
+
+# serializer where the queryset will serialize like convert it to json object to use it in response
+# for example, because some ORM query will return QuerySet not a json object
