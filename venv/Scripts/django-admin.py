2eff9a7c622003cf6e0e5c9629f6c607807260a6
Akshita Jain <akshitajain963@gmail.com>
2023-06-19 17:49:44 +0530

    changes in requirements

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Agrex.ai Assignment\Agrex.ai_Assignment_BE\Agrex\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
